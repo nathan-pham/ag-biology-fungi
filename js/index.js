2cd224c1101d4e79019625ee7e4851e7a2477465
Nathan Pham <nathanpham.me@gmail.com>
2021-04-26 14:29:55 -0700

    animations

--- js/index.js
+++ js/index.js
@@ -1,5 +1,25 @@
 // overlay
-gsap.to(".overlay .panel", { duration: 1, y: "-100%", stagger: 0.3, ease: Expo.easeInOut })
+gsap.to(".overlay .panel", { duration: 0.5, y: "-100%", stagger: 0.1, ease: Expo.easeInOut })
 
 // wavy mushroom
 gsap.fromTo(".image-wrapper img", { transformOrigin: "0% 100%", skewX: "1deg" }, { skewX: "-1deg", repeat: -1, duration: 1, ease: "linear", yoyo: true })
+
+const base = (delay) => ({
+    duration: 1,
+    ease: Expo.easeInOut,
+    delay,
+    opacity: 1,
+    y: 0,
+    x: 0
+})
+
+// main text
+gsap.fromTo(".landing-text h1", { y: 60, opacity: 0 }, base(0.75))
+gsap.fromTo(".landing-text p", { y: 60, opacity: 0 }, base(1))
+gsap.fromTo(".landing-text a", { y: 60, opacity: 0 }, base(1.25))
+
+// navigation
+gsap.fromTo(".navbar li", { y: -100, opacity: 0 }, { ...base(1.5), stagger: 0.1 })
+
+// mushroom
+gsap.fromTo(".image-background", { x: "100vw" }, { ...base(1.75), duration: 2 })
