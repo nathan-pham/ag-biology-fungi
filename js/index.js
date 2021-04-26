c838535c5665ee9e33d381a65029a60d0bc2c72c
Nathan Pham <nathanpham.me@gmail.com>
2021-04-26 14:23:27 -0700

    mushroom, overlay

--- js/index.js
+++ js/index.js
@@ -1,1 +1,5 @@
-gsap.fromTo(".image-wrapper img", { transformOrigin: "0% 100%", skewX: "2deg" }, { skewX: "-2deg", repeat: -1, duration: 1, ease: "linear", yoyo: true })
+// overlay
+gsap.to(".overlay .panel", { duration: 1, y: "-100%", stagger: 0.3, ease: Expo.easeInOut })
+
+// wavy mushroom
+gsap.fromTo(".image-wrapper img", { transformOrigin: "0% 100%", skewX: "1deg" }, { skewX: "-1deg", repeat: -1, duration: 1, ease: "linear", yoyo: true })
